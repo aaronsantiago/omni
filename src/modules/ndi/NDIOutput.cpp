51610ddb068b607b48de183f7dd2857e361ea85d
Jiri Melnikov <jiri@melnikoff.org>
2023-12-21 16:41:09 +0100

    Added support for receiving PTZ commands, currently only pan/tilt is supported.

--- src/modules/ndi/NDIOutput.cpp
+++ src/modules/ndi/NDIOutput.cpp
@@ -19,6 +19,8 @@
 
 #include "yuri/core/utils.h"
 
+#include "rapidxml.hpp"
+
 #include "utils.h"
 
 #include <thread>
@@ -27,150 +29,170 @@
 #include <vector>
 #include <cstring>
 
+#include <iostream>
+#include <fstream>
+
 namespace yuri {
 namespace ndi {
 
 IOTHREAD_GENERATOR(NDIOutput)
 
 core::Parameters NDIOutput::configure() {
-	core::Parameters p = IOThread::configure();
-	p["stream"]["Name of the stream to send."]="Dicaffeine";
-	p["audio"]["Set to true if audio should be send."]=false;
-	p["fps"]["Sets fps indicator sent in the stream"]="";
-	p["ndi_path"]["Path where to find the NDI libraries, if empty, env variable NDI_PATH is used."]="";
-	return p;
+    core::Parameters p = IOThread::configure();
+    p["stream"]["Name of the stream to send."]="Dicaffeine";
+    p["audio"]["Set to true if audio should be send."]=false;
+    p["fps"]["Sets fps indicator sent in the stream"]="";
+    p["ptz"]["Support for PTZ control"]=false;
+    p["ndi_path"]["Path where to find the NDI libraries, if empty, env variable NDI_PATH is used."]="";
+    return p;
 }
 
 NDIOutput::NDIOutput(log::Log &log_,core::pwThreadBase parent, const core::Parameters &parameters)
 :core::IOThread(log_,parent,1,0,std::string("NDIOutput")),
 event::BasicEventProducer(log),event::BasicEventConsumer(log),
-stream_("Dicaffeine"),audio_enabled_(false),fps_(0),ndi_path_("") {
-	IOTHREAD_INIT(parameters)
-	set_latency(10_us);
-	if (audio_enabled_) resize(2,0);
-	// Load NDI library
-	NDIlib_ = load_ndi_library(ndi_path_);
-	// Init NDI
-	if (!NDIlib_->initialize())
-		throw exception::InitializationFailed("Failed to initialize NDI input.");
+stream_("Dicaffeine"),audio_enabled_(false),fps_(0),ptz_(false),ndi_path_("") {
+    IOTHREAD_INIT(parameters)
+    set_latency(10_us);
+    if (audio_enabled_) resize(2,0);
+    // Load NDI library
+    NDIlib_ = load_ndi_library(ndi_path_);
+    // Init NDI
+    if (!NDIlib_->initialize())
+        throw exception::InitializationFailed("Failed to initialize NDI input.");
 }
 
 NDIOutput::~NDIOutput() {
 }
 
 void NDIOutput::run() {
-	NDIlib_send_create_t NDI_send_create_desc;
-	NDI_send_create_desc.p_ndi_name = stream_.c_str();
-	if (audio_enabled_) {
-		NDI_send_create_desc.clock_audio = true;
-	}
-	pNDI_send_ = NDIlib_->send_create(&NDI_send_create_desc);
-	if (!pNDI_send_)
-		throw exception::InitializationFailed("Failed to initialize NDI sender.");
+    NDIlib_send_create_t NDI_send_create_desc;
+    NDI_send_create_desc.p_ndi_name = stream_.c_str();
+    if (audio_enabled_) {
+        NDI_send_create_desc.clock_audio = true;
+    }
+    pNDI_send_ = NDIlib_->send_create(&NDI_send_create_desc);
+    if (!pNDI_send_)
+        throw exception::InitializationFailed("Failed to initialize NDI sender.");
 
-    static const char* p_connection_str =	"<ndi_product long_name=\"NDI Yuri module\" "
-											"             short_name=\"NDI module\" "
-											"             manufacturer=\"Unknown.\" "
-											"             version=\"1.000.000\" "
-											"             session=\"default\" "
-											"             model_name=\"YM\" "
-											"             serial=\"ABCDEFG\"/>";
-    NDIlib_metadata_frame_t NDI_connection_type;
-    NDI_connection_type.length          = (int)::strlen(p_connection_str);
-    NDI_connection_type.timecode        = NDIlib_send_timecode_synthesize;
-    NDI_connection_type.p_data          = (char*)p_connection_str;	// should be const in header file
-	NDIlib_->send_add_connection_metadata(pNDI_send_, &NDI_connection_type);
-	std::thread th(&NDIOutput::sound_sender, this);
-	IOThread::run();
-	stop_stream();
-	th.join();
+    if (ptz_) {
+        NDIlib_metadata_frame_t NDI_capabilities;
+        NDI_capabilities.p_data = strdup("<ndi_capabilities ntk_ptz=\"true\""
+                                         "                  web_control=\"https://dicaffeine.com\""
+                                         "                  ntk_exposure_v2=\"false\"/>");
+        NDIlib_->send_add_connection_metadata(pNDI_send_, &NDI_capabilities);
+    }
+
+    std::thread audio_thread(&NDIOutput::audio_sender, this);
+    std::thread events_thread(&NDIOutput::events_sender, this);
+    IOThread::run();
+    stop_stream();
+    audio_thread.join();
+    events_thread.join();
 }
 
-void NDIOutput::sound_sender() {
-	while (running() && audio_enabled_) {
-		aframe_to_send_ = std::dynamic_pointer_cast<core::RawAudioFrame>(pop_frame(1));
-		if (streaming_enabled_ && aframe_to_send_) {
-			NDIlib_audio_frame_interleaved_16s_t NDI_audio_frame;
-			NDI_audio_frame.no_channels = aframe_to_send_->get_channel_count();
-			NDI_audio_frame.no_samples = aframe_to_send_->get_sample_count();
-			NDI_audio_frame.sample_rate = aframe_to_send_->get_sampling_frequency();
-			NDI_audio_frame.p_data = (short*)aframe_to_send_->data();
-			NDIlib_->util_send_send_audio_interleaved_16s(pNDI_send_, &NDI_audio_frame);
-			aframe_to_send_ = nullptr;
-		} else {
-			ThreadBase::sleep(10_us);
-		}
-	}
+void NDIOutput::audio_sender() {
+    while (running() && audio_enabled_) {
+        aframe_to_send_ = std::dynamic_pointer_cast<core::RawAudioFrame>(pop_frame(1));
+        if (aframe_to_send_) {
+            NDIlib_audio_frame_interleaved_16s_t NDI_audio_frame;
+            NDI_audio_frame.no_channels = aframe_to_send_->get_channel_count();
+            NDI_audio_frame.no_samples = aframe_to_send_->get_sample_count();
+            NDI_audio_frame.sample_rate = aframe_to_send_->get_sampling_frequency();
+            NDI_audio_frame.p_data = (short*)aframe_to_send_->data();
+            NDIlib_->util_send_send_audio_interleaved_16s(pNDI_send_, &NDI_audio_frame);
+            aframe_to_send_ = nullptr;
+        } else {
+            ThreadBase::sleep(10_us);
+        }
+    }
+}
+
+void NDIOutput::events_sender() {
+    NDIlib_metadata_frame_t metadata_cmd;
+    while (running()) {
+        while (NDIlib_->send_capture(pNDI_send_, &metadata_cmd, 0) == NDIlib_frame_type_metadata) {
+            try {
+                // Get the parser
+                std::string xml(metadata_cmd.p_data);
+                rapidxml::xml_document<char> parser;
+                parser.parse<0>((char*)xml.data());
+                // Get the tag
+                rapidxml::xml_node<char>* p_node = parser.first_node();
+                // Check its a node
+                if ((!p_node) || (p_node->type() != rapidxml::node_element)) continue;
+                // Get the values
+                if (!::strcasecmp(p_node->name(), "ntk_ptz_pan_tilt_speed")) {    
+                    const rapidxml::xml_attribute<char>* p_pan_speed = p_node->first_attribute("pan_speed");
+                    const rapidxml::xml_attribute<char>* p_tilt_speed = p_node->first_attribute("tilt_speed");
+                    const float pan_speed = p_pan_speed ? (float)::atof(p_pan_speed->value()) : 0.0f;
+                    const float tilt_speed = p_tilt_speed ? (float)::atof(p_tilt_speed->value()) : 0.0f;
+                    emit_event("pan_speed", pan_speed);
+                    emit_event("tilt_speed", tilt_speed);
+                }
+            } catch (...) {
+                // Not a valid message
+            }
+        }
+        ThreadBase::sleep(10_us);
+    }
 }
 
 bool NDIOutput::step() {
-	// if (!NDIlib_->send_get_no_connections(pNDI_send_, 1000)) {
-	// 	vframe_to_send_ = std::dynamic_pointer_cast<core::RawVideoFrame>(pop_frame(0));
-	// 	streaming_enabled_ = false;
-	// 	return true;
-	// } else {
-		streaming_enabled_ = true;
-	// }
-	auto frame_to_send = pop_frame(0);
-	if (!frame_to_send)
-		return true;
-	if (frame_to_send->get_format() == yuri::core::compressed_frame::h264) {
-		vcmpframe_to_send_ = std::dynamic_pointer_cast<core::VideoFrame>(frame_to_send);
-		log[log::info] << "Compressed frame with res " << vcmpframe_to_send_->get_resolution() << " and " << core::compressed_frame::get_format_name(vcmpframe_to_send_->get_format());
-		streaming_enabled_ = false;
-		return true;
-	} else {
-		vframe_to_send_ = std::dynamic_pointer_cast<core::RawVideoFrame>(frame_to_send);
-		if (!vframe_to_send_)
-			return true;
-		NDIlib_video_frame_v2_t NDI_video_frame;
-		NDI_video_frame.xres = vframe_to_send_->get_width();
-		NDI_video_frame.yres = vframe_to_send_->get_height();
-		NDI_video_frame.FourCC = yuri_format_to_ndi(vframe_to_send_->get_format());
-		NDI_video_frame.line_stride_in_bytes = 0; // autodetect
-		if (fps_ > 0 && fps_ == std::ceil(fps_)) {
-			NDI_video_frame.frame_rate_N = 1000*fps_;
-			NDI_video_frame.frame_rate_D = 1000;
-		} else if (fps_ > 0) {
-			int fps = std::ceil(fps_);
-			NDI_video_frame.frame_rate_N = 1000*fps;
-			NDI_video_frame.frame_rate_D = 1001;
-		}
-		NDIlib_tally_t NDI_tally;
-		NDIlib_->send_get_tally(pNDI_send_, &NDI_tally, 0);
-		NDI_video_frame.p_data = PLANE_RAW_DATA(vframe_to_send_,0);
-		NDIlib_->send_send_video_v2(pNDI_send_, &NDI_video_frame);
-	}
-	return true;
+    auto frame_to_send = pop_frame(0);
+    if (!frame_to_send)
+        return true;
+    if (frame_to_send->get_format() == yuri::core::compressed_frame::h264) {
+        vcmpframe_to_send_ = std::dynamic_pointer_cast<core::VideoFrame>(frame_to_send);
+        log[log::info] << "Compressed frame with resolution " << vcmpframe_to_send_->get_resolution() << " and format \"" << core::compressed_frame::get_format_name(vcmpframe_to_send_->get_format()) << "\" is not supported yet.";
+        return true;
+    } else {
+        vframe_to_send_ = std::dynamic_pointer_cast<core::RawVideoFrame>(frame_to_send);
+        if (!vframe_to_send_)
+            return true;
+        NDIlib_video_frame_v2_t NDI_video_frame;
+        NDI_video_frame.xres = vframe_to_send_->get_width();
+        NDI_video_frame.yres = vframe_to_send_->get_height();
+        NDI_video_frame.FourCC = yuri_format_to_ndi(vframe_to_send_->get_format());
+        NDI_video_frame.line_stride_in_bytes = 0; // autodetect
+        if (fps_ > 0 && fps_ == std::ceil(fps_)) {
+            NDI_video_frame.frame_rate_N = 1000*fps_;
+            NDI_video_frame.frame_rate_D = 1000;
+        } else if (fps_ > 0) {
+            int fps = std::ceil(fps_);
+            NDI_video_frame.frame_rate_N = 1000*fps;
+            NDI_video_frame.frame_rate_D = 1001;
+        }
+        NDIlib_tally_t NDI_tally;
+        NDIlib_->send_get_tally(pNDI_send_, &NDI_tally, 0);
+        NDI_video_frame.p_data = PLANE_RAW_DATA(vframe_to_send_,0);
+        NDIlib_->send_send_video_v2(pNDI_send_, &NDI_video_frame);
+    }
+    return true;
 }
 
 void NDIOutput::stop_stream() {
-	NDIlib_->send_destroy(pNDI_send_);
-	NDIlib_->destroy();
-}
-
-void NDIOutput::emit_events() {
-	// Nothing here now
+    NDIlib_->send_destroy(pNDI_send_);
+    NDIlib_->destroy();
 }
 
 bool NDIOutput::do_process_event(const std::string& event_name, const event::pBasicEvent& event) {
-	if (iequals(event_name, "quit")) {
+    if (iequals(event_name, "quit")) {
         request_end(core::yuri_exit_interrupted);
         return true;
     }
-	log[log::info] << "Got unknown event \"" << event_name << "\", timestamp: " << event->get_timestamp();
-	return false;
+    log[log::info] << "Got unknown event \"" << event_name << "\", timestamp: " << event->get_timestamp();
+    return false;
 }
 
 bool NDIOutput::set_param(const core::Parameter &param) {
-	if (assign_parameters(param)
-			(stream_, "stream")
-			(audio_enabled_, "audio")
-			(fps_, "fps")
-			(ndi_path_, "ndi_path")
-			)
-		return true;
-	return IOThread::set_param(param);
+    if (assign_parameters(param)
+            (stream_,        "stream")
+            (audio_enabled_, "audio")
+            (fps_,           "fps")
+            (ptz_,           "ptz")
+            (ndi_path_,      "ndi_path"))
+        return true;
+    return IOThread::set_param(param);
 }
 
 }
--- src/modules/ndi/NDIOutput.h
+++ src/modules/ndi/NDIOutput.h
@@ -23,33 +23,32 @@ namespace ndi {
 
 class NDIOutput:public core::IOThread, public event::BasicEventProducer, public event::BasicEventConsumer {
 public:
-	NDIOutput(log::Log &log_,core::pwThreadBase parent, const core::Parameters &parameters);
-	virtual ~NDIOutput() noexcept;
-	virtual void run() override;
-	IOTHREAD_GENERATOR_DECLARATION
-	static core::Parameters configure();
-	void sound_sender();
+    NDIOutput(log::Log &log_,core::pwThreadBase parent, const core::Parameters &parameters);
+    virtual ~NDIOutput() noexcept;
+    virtual void run() override;
+    IOTHREAD_GENERATOR_DECLARATION
+    static core::Parameters configure();
+    void audio_sender();
+    void events_sender();
 private:
-	bool step();
-	virtual bool set_param(const core::Parameter &param) override;
-	virtual bool do_process_event(const std::string& event_name, const event::pBasicEvent& event);
+    bool step();
+    virtual bool set_param(const core::Parameter &param) override;
+    virtual bool do_process_event(const std::string& event_name, const event::pBasicEvent& event);
 
-	void stop_stream();
+    void stop_stream();
 
-	void emit_events();
+    std::string stream_;
+    bool audio_enabled_;
+    float fps_;
+    bool ptz_;
+    std::string ndi_path_;
 
-	std::string stream_;
-	bool audio_enabled_;
-	bool streaming_enabled_;
-	float fps_;
-	std::string ndi_path_;
+    const NDIlib_v5* NDIlib_;
+    NDIlib_send_instance_t pNDI_send_;
 
-	const NDIlib_v5* NDIlib_;
-	NDIlib_send_instance_t pNDI_send_;
-
-	core::pRawAudioFrame aframe_to_send_;
-	core::pRawVideoFrame vframe_to_send_;
-	core::pVideoFrame vcmpframe_to_send_;
+    core::pRawAudioFrame aframe_to_send_;
+    core::pRawVideoFrame vframe_to_send_;
+    core::pVideoFrame vcmpframe_to_send_;
 };
 
 }
